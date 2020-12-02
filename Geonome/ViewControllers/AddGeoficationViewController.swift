541edfa9cd2beaee1cfdc84f876c44971f859e50
Christian Stevanus <chris.stev30@gmail.com>
2020-12-02 16:12:48 +0700

    add UI for geofication

---
+++ Geonome/ViewControllers/AddGeoficationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddGeoficationViewController.swift
+//  Geonome
+//
+//  Created by Chris Stev on 02/12/20.
+//  Copyright © 2020 Setel. All rights reserved.
+//
+
+import UIKit
+
+class AddGeoficationViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 3
+//    }
+
+}
